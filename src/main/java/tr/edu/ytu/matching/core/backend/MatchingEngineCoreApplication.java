6ba9411afdbfe4c4f5b701ac2c65919573f6f493
enes-ozbilgin <eneseminozbilgin@gmail.com>
2026-03-09 20:47:52 +0300

    Proje taslağı kuruldu.

---
+++ src/main/java/tr/edu/ytu/matching/core/backend/MatchingEngineCoreApplication.java
@@ -0,0 +1,13 @@
+package tr.edu.ytu.matching.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingEngineCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchingEngineCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tr/edu/ytu/matching/core/MatchingEngineCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.edu.ytu.matching.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingEngineCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
